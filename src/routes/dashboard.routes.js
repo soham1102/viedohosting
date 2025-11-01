2f62a82f5db6a883aabfc2bfbabb7796a7cba1d7
soham1102 <sohamsharma1102@gmail.com>
2025-11-01 20:56:07 +0530

    dashboard completed successfully

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getChannelStats, getChannelVideos } from "../controllers/dashboard.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router()
-router.route("/get-channel-stats").get(verifyJWT,getChannelStats)
-router.route("/get-channel-details").get(verifyJWT,getChannelVideos)
+router.route("/get-channel-stats/:channelId").get(verifyJWT,getChannelStats)
+router.route("/get-channel-viedos/:channelId").get(verifyJWT,getChannelVideos)
 
 export default router
